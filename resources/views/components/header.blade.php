0de81e28661229d72d3bf6c4ba2223fdff8c92e3
Henzo3110 <henzogabrielmendesdemattos@gmail.com>
2024-05-23 15:17:41 -0300

    Tentando começar sapoha

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+
+<div class="w-full bg-gradient-to-br from-purple-700 via-purple-500 to-purple-300 h-24 fixed" >
+    <div class="flex justify-start items-center">
+        <img src="" alt="">
+    </div>
+    <div class="flex justify-around items-center">
+        <x-fas-home class="w-20 h-20 text-fuchsia-300"/>
+        <x-fas-shop class="w-20 h-20 text-fuchsia-300"/>
+        <x-fas-plus class="w-20 h-20 text-fuchsia-300" />
+    </div>
+</div>
